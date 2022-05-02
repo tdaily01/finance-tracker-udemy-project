5527af0464ac5986f91f9e5818aa56aaf6a473d4
Taylor <tdaily0196@gmail.com>
2022-05-02 17:37:48 -0400

    delete stocks from a user's profile and add first and last names to users

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -10,5 +10,13 @@ class UserStocksController < ApplicationController
         flash[:notice] = "Stock #{stock.name} was added to your portfolio."
         redirect_to my_portfolio_path
     end
+
+    def destroy
+        stock = Stock.find(params[:id])
+        current_user.stocks.delete(stock)
+
+        flash[:notice] = "#{stock.name} was deleted from your list"
+        redirect_to my_portfolio_path
+    end
     
 end
