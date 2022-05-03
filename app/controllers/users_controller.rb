302a2fdbdab144232145e1cd3d5de195da1ab64f
Taylor <tdaily0196@gmail.com>
2022-05-03 10:46:21 -0400

    Project Completion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,40 @@
 class UsersController < ApplicationController
 
+  def my_friends
+    @friends = current_user.friends
+  end
+
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
+  end
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+          respond_to do |format|
+              format.js { render partial: 'users/friend_result' }
+          end
+      else
+          respond_to do |format|
+              flash.now[:alert] = "Please enter a valid symbol"
+              format.js {render partial: 'users/friend_result'}
+          end
+
+      end
+    else
+      respond_to do |format|
+          flash.now[:alert] = "Please enter a symbol"
+          format.js {render partial: 'users/friend_result'}
+      end
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
   
 end
